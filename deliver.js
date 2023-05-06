2b7015b43163d643602ad25d05b3e7c012642f53
OttCS <ottcs20@gmail.com>
2023-05-06 16:08:45 -0400

    Deliver

---
+++ deliver.js
@@ -0,0 +1,11 @@
+document.body.innerHTML = `<header>
+<h1>Hello World!</h1>
+<p>My name is Maddie, and I'm pretty cool</p>
+<p>Please hire me</p>
+<noscript>This page requires scripts to run</noscript>
+</header>
+<nav>
+<a href="#">First Link</a>
+<a href="#">Another Link</a>
+<a href="#">Final Link</a>
+</nav>` + document.body.innerHTML;
