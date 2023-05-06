aac5e44fb5d0aa8e949d5275384728e696fbc407
OttCS <ottcs20@gmail.com>
2023-05-06 19:06:09 -0400

    JS Links

--- deliver.js
+++ deliver.js
@@ -1,6 +1,26 @@
-document.querySelector("nav").innerHTML = `
-<a href="index.html" class="bubble">BIO</a>
-<a href="portfolio.html" class="bubble">PORTFOLIO</a>
-<a href="files/resume.pdf" target="_blank" class="bubble">RESUME</a>
-<a href="contact.html" class="bubble">CONTACT</a>
-`;
+let nav = document.querySelector("nav");
+[
+  ["index.html", "BIO"],
+  ["portfolio.html", "PORTFOLIO"],
+  ["files/resume.pdf", "RESUME"],
+  ["contact.html", "CONTACT"],
+].forEach(link=>{
+  let a = document.createElement('a');
+  a.href = link[0];
+  a.innerText = link[1];
+  a.classList.add('bubble');
+  if (!link[0].includes('.html'))
+    a.target = "_blank";
+  console.log(a.href, window.location.pathname);
+  // if (a.href == (window.location.pathname == '/' ? 'index.html' : window.location.pathname)) {
+  //   a.classList.add("active");
+  // }
+  nav.appendChild(a);
+});
+
+// document.querySelector("nav").innerHTML = `
+// <a href="index.html" class="bubble">BIO</a>
+// <a href="portfolio.html" class="bubble">PORTFOLIO</a>
+// <a href="files/resume.pdf" target="_blank" class="bubble">RESUME</a>
+// <a href="contact.html" class="bubble">CONTACT</a>
+// `;
