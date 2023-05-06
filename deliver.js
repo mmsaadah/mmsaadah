fefbb3f3172751b66aafca88b92d28327ec0bd99
OttCS <ottcs20@gmail.com>
2023-05-06 18:52:58 -0400

    75%

--- deliver.js
+++ deliver.js
@@ -1,16 +1,6 @@
-document.body.innerHTML = `
-<header>
-<img class="headshot" src="photos/headshot.jpg"></img>
-<div class="bubble">
-  <h1>Madelyn Saadah</h1>
-  <h3>Public and Media Relations</h3>
-  <p>Here's a little bit about me and my work!</p>
-</div>
-</header>
-<nav>
+document.querySelector("nav").innerHTML = `
 <a href="index.html" class="bubble">BIO</a>
 <a href="portfolio.html" class="bubble">PORTFOLIO</a>
-<a href="files/resume.pdf" class="bubble">RESUME</a>
+<a href="files/resume.pdf" target="_blank" class="bubble">RESUME</a>
 <a href="contact.html" class="bubble">CONTACT</a>
-</nav>
-` + document.body.innerHTML;
+`;
